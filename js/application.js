3937dd79bb2f6710b8bd96494affbba3593d70fd
leshayface <leshay@Leshays-MacBook-Pro.local>
2016-07-11 09:21:11 +0200

    Form and second NAV

--- js/application.js
+++ js/application.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
   $('#container').fullpage({
+    menu: '#menu',
+		anchors: ['start', 'aboutCar', 'instantOffer', 'freeInspection', 'sameDay']
   });
 
   $("#home h3").fitText(0.8, { maxFontSize: '24px' });
+
+  $("#make-model").focus(function(){
+    console.log('yes');
+    $(".form_container").children().removeClass("home_form_hidden");
+    $(".form_container").children().children().removeClass("home_form_hidden");
+  });
 
   $('.moveSectionDown').click(function(e){
     e.preventDefault();
